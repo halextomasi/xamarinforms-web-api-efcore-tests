343b1381e43a61eeee058982f908f818c7980457
Hálex Tomasi <halextomasi@gmail.com>
2018-09-30 21:52:06 -0300

    Migration success.

---
+++ Controle.API/Migrations/20181001004917_NumeroDependentes.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Controle.API.Migrations
+{
+    public partial class NumeroDependentes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "NumeroDependentes",
+                table: "Contribuintes",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NumeroDependentes",
+                table: "Contribuintes");
+        }
+    }
+}
